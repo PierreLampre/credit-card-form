538dbd2156ba7062a21d94b5ee3a56fd9ab287a6
Justin <pierrelampre@gmail.com>
2021-01-22 14:52:20 -0500

    added media query for height to maintain element proportions at differeing screen sizes.

--- src/components/CreditCardForm/CreditCardForm.js
+++ src/components/CreditCardForm/CreditCardForm.js
@@ -15,13 +15,36 @@ const CreditCardForm = () => {
         setStrings({ ...strings, [e.target.name]: e.target.value });
     }
 
+    let ccNum = document.getElementById("ccNum");
+    let expiry = document.getElementById("expiry");
+    let fullName = document.getElementById("fullName");
+    let cvv = document.getElementById("ccv");
+
     function changeStringsAddWhiteSpace(e) {
         setStrings({ ...strings, [e.target.name]: e.target.value.replace(/(\d{4})/g, '$1 ').replace(/(^\s+|\s+$)/,'')});
-        let ccNum = document.getElementById("ccNum");
         
+        let ccNum = document.getElementById("ccNum");
         //HTML5 validation ignores the maxlength property when type is number :(
         //The if statement provides desired maxlength limit when type="number"
         if (ccNum.value.length > 15) ccNum.value = ccNum.value.slice(0, 15);
+    }
+
+    function invalidInput(el, message) {
+        el.style.border = "4px solid red";
+        el.placeholder = message
+    }
+
+    function validateStrings(e, ccNum, expiry, fullName, cvv) {
+        e.preventDefault();
+        if(ccNum.value.length < 14) {
+            invalidInput(ccNum, "Credit card number entered is too short.");
+        } else if (expiry.value.length < 5){
+            invalidInput(expiry, "Format = MM/YY");
+        } else if(fullName.value.length < 2){
+            invalidInput(fullName, "Please Enter Full Name");
+        } else if(cvv.value.length < 3 || cvv.value.length > 5) {
+            invalidInput(cvv, "Invalid CCV")
+        }
     }
 
     return (
@@ -38,6 +61,7 @@ const CreditCardForm = () => {
                                 name="ccNum"
                                 id="ccNum"
                                 className="input-big"
+                                placeholder="Enter Your CC Number"
                                 onChange={changeStringsAddWhiteSpace}
                                 required
                             />
@@ -47,7 +71,9 @@ const CreditCardForm = () => {
                             <input
                                 type="text"
                                 name="expiry"
+                                id="expiry"
                                 className="input-small"
+                                placeholder="MM/YY"
                                 onChange={changeStrings}
                                 required
                             />
@@ -61,6 +87,8 @@ const CreditCardForm = () => {
                                 type="text"
                                 name="name"
                                 className="input-big"
+                                id="fullName"
+                                placeholder="Your name"
                                 onChange={changeStrings}
                                 required
                             />
@@ -68,9 +96,11 @@ const CreditCardForm = () => {
                         <div className="row">
                             <label>CVV</label>
                             <input
-                                type="text"
+                                type="number"
                                 name="cvv"
+                                id="ccv"
                                 className="input-small"
+                                placeholder="***"
                                 required
                             />
                         </div>
@@ -79,7 +109,18 @@ const CreditCardForm = () => {
 
 
                     <div className="button-box">
-                        <button id="submit">Submit</button>
+                        <button 
+                        id="submit"
+                        onClick={(e) => validateStrings(
+                            e,
+                            ccNum, 
+                            expiry,
+                            fullName,
+                            cvv
+                        )}
+                        >
+                            Submit
+                        </button>
                         <button id="clear">Clear</button>
                     </div>
 
